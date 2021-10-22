a8f1b175f101d48bd1b5ee4ba0941d48fe79857e
Marek Korpa <72918109+Markildas@users.noreply.github.com>
2021-10-22 12:51:02 +0200

    merged to one folder

--- chromeExtension/practice innerhtml/index.js
+++
@@ -1,8 +0,0 @@
-// Use .innerHTML to render a Buy! button inside the div container
-const cointainer = document.getElementById("container")
-
-cointainer.innerHTML = "<button onclick='buy()'> Buy! </button>"
-
-function buy() {
-    cointainer.innerHTML += "<p>Thank you for buying!</p>"
-}
