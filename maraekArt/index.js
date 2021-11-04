68a6c3a64b77c8dd9d820562a3a2851f9043dbe9
Marek Korpa <72918109+Markildas@users.noreply.github.com>
2021-11-04 19:12:42 +0100

    changes

--- maraekArt/index.js
+++ maraekArt/index.js
@@ -1,13 +1,4 @@
 let headingEl = document.getElementById("heading-el")
 let buttonEl = document.getElementById("button-el")
 
-function thirdEye() {
-    headingEl.innerHTML=(`
-    <img src="tool.png" height="150px">
-    `)
-}
-
-buttonEl.addEventListener("dblclick", function resetHeading() {
-    headingEl.textContent = ("maræk.art")
-})
 
